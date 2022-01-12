b7456af45a4b5922f3b5c3ce60b3863a66aa475f
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2022-01-12 16:03:52 +0530

    Create insertintoabst.cpp

---
+++ 01. January/insertintoabst.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (!root) {
+            TreeNode *newNode = new TreeNode(val);
+            return newNode;
+		}
+        if(val < root->val) root->left = insertIntoBST(root->left, val);
+        
+        else root->right = insertIntoBST(root->right, val);
+        
+        return root;
+    }
+};
