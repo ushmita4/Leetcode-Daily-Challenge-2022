db71e4cb819dff53df7d3fb75a69b21dfaf5de4c
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2022-02-15 20:01:07 +0530

    Create singlenumber.cpp

---
+++ 02. February/singlenumber.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int single=nums[0];
+        for(int i=1;i<nums.size();i++)
+            single=single^nums[i];
+        
+        return single;
+    }
+};
