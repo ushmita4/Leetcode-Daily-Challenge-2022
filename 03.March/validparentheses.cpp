194538369c2baf71e19fd941e52338339c41d6d5
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2022-03-13 18:01:26 +0530

    Create validparentheses.cpp

---
+++ 03.March/validparentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char> st;
+       
+        for(int i = 0; i < s.size(); i++){
+            
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+            }
+            
+            else if(s[i] == ')'){
+                
+                if(st.empty()) return false;
+                
+                else if(st.top() != '(') return false;
+                
+                else st.pop();
+            }
+            
+            else if(s[i] == ']'){
+                if(st.empty()) return false;
+                else if(st.top() != '[') return false;
+                else st.pop();
+            }
+            else if(s[i] == '}'){
+                if(st.empty()) return false;
+                else if(st.top() != '{') return false;
+                else st.pop();
+            }
+        }
+        
+        if(st.empty()) return true; 
+        return false; 
+    }
+};
