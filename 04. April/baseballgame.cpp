43817ae57a32c9bcbd80ab9b99732e811f897e19
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2022-04-10 21:52:51 +0530

    Create baseballgame.cpp

---
+++ 04. April/baseballgame.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    int calPoints(vector<string> &ops)
+    {
+        
+        vector<int> ans;
+        int n = ops.size();
+
+        
+        for (int i = 0; i < n; i++)
+        {
+            if (ops[i] == "D")
+                ans.push_back(2 * ans.back());
+            else if (ops[i] == "C")
+                ans.pop_back();
+            else if (ops[i] == "+")
+                ans.push_back(ans[ans.size() - 1] + ans[ans.size() - 2]);
+            else
+                ans.push_back(stoi(ops[i]));
+        }
+        // adding the remaining integers in vector
+        //  after performing all the operations
+        int sum = 0;
+        for (int i : ans)
+            sum += i;
+
+        // returning answer as sum
+        return sum;
+    }
+};
