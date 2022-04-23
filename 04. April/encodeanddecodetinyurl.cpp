8444df684cc0c5dedbba746442e34658a1c790df
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2022-04-23 21:37:55 +0530

    Create encodeanddecodetinyurl.cpp

---
+++ 04. April/encodeanddecodetinyurl.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+	string encode(string longUrl) {
+		return longUrl;
+	}
+	string decode(string shortUrl) {
+	   return shortUrl;   
+	}
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
