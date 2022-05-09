e0f298ba4666fb160de749e6b5c82df0c636bf04
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2022-05-09 21:26:17 +0530

    Create lettercombinationsofaphonenumber.cpp

---
+++ 05. May/lettercombinationsofaphonenumber.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty()) return {};
+        
+        unordered_map <char,string> num_to_char{
+            {'2',"abc"}, {'3',"def"}, 
+            {'4',"ghi"}, {'5',"jkl"}, {'6',"mno"},
+            {'7',"pqrs"},{'8',"tuv"}, {'9',"wxyz"}
+        };
+        
+        vector<string> result;
+        string combination(digits.size(), ' ');
+        letterCombination(digits, combination, 0, num_to_char, result);
+        return result;        
+    }
+    
+    void letterCombination(string digits, string &combination, int idx, unordered_map <char,string> &num_to_char, vector<string> &result){
+        if(idx == digits.length()){
+            result.push_back(combination);
+            return;
+        }
+        
+        for(char c:num_to_char[digits[idx]]){
+            combination[idx] = c;
+            letterCombination(digits, combination, idx+1, num_to_char, result);
+        }
+    }
+};
