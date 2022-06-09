e5a0926374f51bb5fe74cb55384e1d83418fdee0
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2022-06-09 23:19:05 +0530

    Create twosum2inputearrayissorted.cpp

---
+++ 06. June/twosum2inputearrayissorted.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int tar) 
+    {
+        //define start and end pointer
+        int s=0, e=nums.size()-1;
+        
+        //iterate until both pointers reach at the same index
+        while(s<e)
+        {
+            if(nums[s]+nums[e]==tar) break;
+            
+            else if(nums[s]+nums[e]>tar) e--;
+            
+            else s++;
+        }
+        //return the index of s and e added by one
+        return {s+1, e+1};           
+    }
+};
