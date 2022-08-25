d2955048f0e0da7c7a57d43e6dea0c1df7d0c0e0
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2022-08-25 21:42:44 +0530

    Create ransomnote.cpp

---
+++ 08. August/ransomnote.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        
+        unordered_map<char, int> m;
+        
+        
+        for(auto x: magazine){
+            m[x]++;
+        }
+        
+		
+        for(auto x: ransomNote){
+		
+            if(!m[x]--){
+                return false;
+            }
+        }
+        return true;
+    }
+};
