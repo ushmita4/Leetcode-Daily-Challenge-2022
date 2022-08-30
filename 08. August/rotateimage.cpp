a95d64ec7457ba87e4f90dad5a4ef84539456ae4
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2022-08-30 19:41:49 +0530

    Create rotateimage.cpp

---
+++ 08. August/rotateimage.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+        
+        // take transpose of the matrix
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j <= i; j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        // reverse the every row of the matrix
+        
+        for(int i = 0; i < n; i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
