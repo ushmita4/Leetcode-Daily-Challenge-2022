58b5e5c6e1d02dbd17d386627b715a3c455091d6
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2022-09-02 10:51:19 +0530

    Create averagelevelsinbinarytree.cpp

---
+++ 09. September/averagelevelsinbinarytree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+       vector<double> answer;
+       queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            long long temp=0;
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode* node=q.front();
+                q.pop();
+                if(node->left!=NULL)
+                    q.push(node->left);
+                if(node->right!=NULL)
+                    q.push(node->right);
+                
+                temp=temp+node->val;
+            }
+            answer.push_back((double)temp/size);
+        }
+        return answer;
+    }
+};
