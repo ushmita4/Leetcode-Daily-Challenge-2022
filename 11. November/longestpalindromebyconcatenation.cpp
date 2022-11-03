7080c38f5fdac1e4f9d2d22c2d7d70dc200be2ff
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2022-11-03 19:39:53 +0530

    Create longestpalindromebyconcatenation.cpp

---
+++ 11. November/longestpalindromebyconcatenation.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        unordered_map<string,int> mp;
+        bool flag=false;
+        int ans=0;
+        
+        for(int i=0;i<words.size();i++){
+            mp[words[i]]++;
+        }
+        
+        for(auto it:words)
+        {
+            string temp=it;
+            reverse(temp.begin(),temp.end());
+            if(temp!=it && mp[temp]>0 && mp[it]>0)
+            {
+                mp[temp]--;
+                mp[it]--;
+                ans+=4;
+            }
+              else if(temp==it && mp[it]>1)
+            {
+                mp[it]-=2;
+                ans+=4;
+            }
+            else if(temp==it && !flag && mp[it]>0)
+            {
+                mp[it]--;
+                flag=true;
+                ans+=2;
+            }
+        }
+       return ans; 
+    }
+};
