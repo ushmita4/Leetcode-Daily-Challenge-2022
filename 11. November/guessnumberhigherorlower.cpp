c59ed66f8c176b6abedde32303adf45811dae980
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2022-11-16 22:49:13 +0530

    Create guessnumberhigherorlower.cpp

---
+++ 11. November/guessnumberhigherorlower.cpp
@@ -0,0 +1,23 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+      int minval = 1, maxval= n, medval;
+        while(minval < maxval) {
+            medval= minval + (maxval - minval)/2;
+            int val = guess(medval);
+            if(val > 0) minval = medval + 1;
+            else if(val < 0) maxval= medval - 1;
+            else return medval;
+        }
+        return minval;   
+    }
+};
